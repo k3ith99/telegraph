6ac2a4cf67b6643d3ade62c36103dedccb58a531
k3ith99 <hamza_saleh1999@hotmail.com>
2022-02-16 14:53:33 +0000

    minor changes

--- client/static/js/script.js
+++ client/static/js/script.js
@@ -7,41 +7,72 @@ const url = ""
 const postContainer = document.getElementById("post-container")
 const singlePost = document.getElementById("single-post")
 
-const getStory = async(e) => {
-    e.preventDefault();
-    let data = await fetch("").json();
-    for(let i in data.posts){
-        let newUl = document.createElement("ul")
 
-        for(let i in data.posts[x]){
-            let post = data.posts[x]
-            let newLi = document.createElement("li")
-            if(i!="id"){
-            newLi.textContent = post[i]
-            newUl.appendChild(newLi)
-            postContainer.appendChild(newUl)
-            }}
-            }
-            submitForm.addEventListener("submit", (e)=>{
-                addPost(e)
-            });
+submit.addEventListener('submit', sendData)
+
+
+async function sendData(e) {
+    e.preventDefault()
+    try {
+        const options = {
+            method: 'POST',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(Object.fromEntries(new FormData(e.target)))
         }
-
-const addStory = async(e) => {
-    e.preventDefault();
-    const postData = {
-        title: e.target.title.value,
-        pseudonym: e.target.pseudonym.value,
-        story: e.target.story.value,
-    };
-    const options = {
-        method: "POST",
-        body: JSON.stringify(postData),
-        headers: {
-            "Content-Type": "application/json" }
+        console.log(options.body)
+        const res = await fetch('http://localhost:3000/posts', options);
+        const { id, err } = await res.json();
+        if(err) { 
+            throw Error(err) 
+        } else {
+            document.querySelector('#title').value = ''
+            document.querySelector('#pseudonym').value = ''
+            document.querySelector('#story').value = ''
+            window.location.hash = `#${id}`
+            updateContent()
+        }
+    } catch(err) {
+        console.log(err)
     }
-    fetch('http://localhost:3000/posts', options)
-    .then(r => r.json())
-    .then(() => e.target.reset())
-    .catch(console.error)
+}
+
+async function getData() {
+    try {
+        // console.log("hello")
+        let res = await fetch('http://localhost:3000/posts')
+        // console.log(res)
+        let data = await res.json()
+        console.log(data)
+        // console.log(data.posts)
+        if (!data) { throw new Error("no posts")}
+        data.forEach(post => {
+            drawPost(post)
+        });
+    } catch(err) {
+        console.log(err)
+    }
+}
+
+
+
+function drawPost(data) {
+    let card = document.createElement('div')
+    let title = document.createElement('h2')
+    let pseudonym = document.createElement('h3')
+    let story = document.createElement('p')
+
+    title.textContent = data.title
+    title.classList.add("title")
+    pseudonym.textContent = data.pseudonym
+    pseudonym.classList.add("pseudonym")
+    story.textContent = data.story
+    story.classList.add("story")
+
+    card.append(title)
+    card.append(pseudonym)
+    card.append(story)
+
+    let main = document.querySelector('main')
+    main.innerHTML = ""
+    main.append(card)
 }
