31bdb0cdefd245611d88f4e0337f3a992318dab5
k3ith99 <hamza_saleh1999@hotmail.com>
2022-02-16 11:25:44 +0000

    html and css done

---
+++ client/static/js/layout.js
@@ -0,0 +1,5 @@
+const form = document.getElementById('form');
+const title = document.getElementById('title');
+const ps = document.getElementById('pseudonym');
+const story = document.getElementById('story')
+
