86da898057aebd01d83149e7539f8795917f6bbd
Dafe-akaka <dafe.akaka123@gmail.com>
2022-02-16 12:44:34 +0000

    created routes

--- server-sql/api/server.js
+++ server-sql/api/server.js
@@ -5,8 +5,10 @@ const server = express();
 server.use(cors());
 server.use(express.json());
 
+const postRoutes = require('./routes/posts')
+server.use('/post', postRoutes)
 
 // Root route
-server.get('/', (req, res) => res.send('Hello, world!'))
+server.get('/', (req, res) => res.send('Welcome to telegraph '))
 
 module.exports = server
